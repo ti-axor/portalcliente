6b03b0817e95f2d642223ac3286e5d43000feadb
tiAxor <ti@axor.com.br>
2023-02-15 16:49:54 -0300

    feat: react-router-dom, page, components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 
 function App() {
   return (
     <Routes>
+      <Route exact path='/portaldocliente' element={ <Home /> } />
       <Route exact path="/" element={ <Home /> } />
-      <Route exact path="/portaldocliente" element={ <Home /> } />
+      <Route path="*" element={ <Navigate replace to="/portaldocliente" /> } />
     </Routes>
   );
 }
