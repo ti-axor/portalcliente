64560b128cafab012ac318220c5e6feb36c3cedb
tiAxor <ti@axor.com.br>
2023-02-23 13:54:35 -0300

    fix: rename folder

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Header from '../../component/Header';
-import Main from '../../component/Main';
-import Footer from '../../component/Footer';
+import Header from '../../components/Header';
+import Main from '../../components/Main';
+import Footer from '../../components/Footer';
 import '../../App.css';
 
 function Home() {
